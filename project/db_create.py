a85095a2e0ca89cebe1e6c0cbb96aa464b82c6a3
Jose Bonilla <josepablob@gmail.com>
2016-03-05 18:22:31 -0500

    Database manager finished, views.py v1 finished

--- project/db_create.py
+++ project/db_create.py
@@ -1,7 +1,7 @@
 # project/db_create.py
 
 import sqlite3
-from config import DATABASE_PATH
+from _config import DATABASE_PATH
 
 with sqlite3.connect(DATABASE_PATH) as connection:
 
@@ -9,4 +9,8 @@ with sqlite3.connect(DATABASE_PATH) as connection:
     c = connection.cursor()
 
     # create the table
-    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT))
+    c.execute("""CREATE TABLE tasks(task_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, due_date TEXT NOT NULL, priority INTEGER NOT NULL, status INTEGER NOT NULL)""")
+
+    # insert dummy data into the table
+    c.execute( 'INSERT INTO tasks (name, due_date, priority, status)' 'VALUES("Finish Real Python Course 2", "03/25/2015", 10, 1)')
+    
