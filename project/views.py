bbac7f07197b01d2dd958e4c0593923a0317030e
Jose Bonilla <josepablob@gmail.com>
2016-03-06 19:12:22 -0500

    added html base and base css styles, added run.py

--- project/views.py
+++ project/views.py
@@ -3,7 +3,7 @@
 import sqlite3
 from functools import wraps
 
-from flask import Flask, flash redirect, render_template, \
+from flask import Flask, flash, redirect, render_template, \
     request, session, url_for
 
 # config
