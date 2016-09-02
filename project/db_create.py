212eb65feb58dce1953c1ab255b8264d068a8875
Jose Bonilla <josepablob@gmail.com>
2016-09-02 17:05:38 -0400

    added users auth

--- project/db_create.py
+++ project/db_create.py
@@ -9,8 +9,8 @@ from datetime import date
 db.create_all()
 
 # insert data
-db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, 1))
-db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, 1))
+#db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, 1))
+#db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, 1))
 
 # commit changes
 db.session.commit()
